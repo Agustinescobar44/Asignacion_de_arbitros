dd042ba2bb222214270933fed9d3817ed7abcd64
agustinesco <84880207+agustinesco@users.noreply.github.com>
2021-10-19 20:27:36 -0300

    agregado GSON al proyecto

--- tp3_progra3/src/metodos/Principal.java
+++ tp3_progra3/src/metodos/Principal.java
@@ -7,7 +7,7 @@ public class Principal {
 
 	public static void main(String[] args) {
 		Equipo a = new Equipo("El colo colo", 10);
-		Equipo b = new Equipo("El trueno verde", 10);
+		Equipo b = new Equipo("Sacachispas", 10);
 		Equipo[] equipos = new Equipo[2];
 		equipos[0] = a;
 		equipos[1] = b;
