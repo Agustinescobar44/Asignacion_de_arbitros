4f3a5c69a234dc5b720c431d6a9caa83fa2b1a5a
agustinesco <84880207+agustinesco@users.noreply.github.com>
2021-10-19 19:42:53 -0300

    Arreglada ruta en principal

--- tp3_progra3/src/metodos/Principal.java
+++ tp3_progra3/src/metodos/Principal.java
@@ -7,12 +7,12 @@ public class Principal {
 
 	public static void main(String[] args) {
 		Equipo a = new Equipo("El colo colo", 10);
-		Equipo b = new Equipo("El sacachispas", 10);
+		Equipo b = new Equipo("El trueno verde", 10);
 		Equipo[] equipos = new Equipo[2];
 		equipos[0] = a;
 		equipos[1] = b;
 		Fecha f = new Fecha(equipos);
-		f.generarJSON("\\tp3_progra3\\tp3_progra3\\src\\archivos\\prueba");
+		f.generarJSON("src\\archivos\\prueba");
 	}
 
 }
