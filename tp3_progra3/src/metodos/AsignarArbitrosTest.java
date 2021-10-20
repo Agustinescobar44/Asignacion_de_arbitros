e59a2cb21a78eded2730d99d0cbf89c595b0f958
agustinesco <84880207+agustinesco@users.noreply.github.com>
2021-10-19 21:27:18 -0300

    Agregado metodo AsignarArbitro y su test

---
+++ tp3_progra3/src/metodos/AsignarArbitrosTest.java
@@ -0,0 +1,28 @@
+package metodos;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import estructuraDeDatos.Equipo;
+import estructuraDeDatos.Partido;
+
+public class AsignarArbitrosTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void testAsignarArbitro() {
+		Equipo a = new Equipo("Boca", 2);
+		Equipo b = new Equipo("river", 2);
+		
+		Partido partido = new Partido(a, b);
+		
+		
+		
+	}
+
+}
---
+++ tp3_progra3/src/test/AsignarArbitros.java
@@ -0,0 +1,10 @@
+package test;
+
+import estructuraDeDatos.Fecha;
+
+public class AsignarArbitros {
+	Fecha[] fechas = null;
+	
+	
+	
+}
