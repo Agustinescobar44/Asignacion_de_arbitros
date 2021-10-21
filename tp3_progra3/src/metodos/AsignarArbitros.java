85cdbe5028dd3a430209644b1ad4ebbba1b4e418
agustinesco <84880207+agustinesco@users.noreply.github.com>
2021-10-21 20:39:14 -0300

    Primer acercamiento a la implementacion

--- tp3_progra3/src/metodos/AsignarArbitros.java
+++ tp3_progra3/src/metodos/AsignarArbitros.java
@@ -1,10 +1,46 @@
-package test;
+package metodos;
 
+import java.util.HashSet;
+import java.util.Set;
+
+import estructuraDeDatos.Equipo;
 import estructuraDeDatos.Fecha;
+import estructuraDeDatos.Partido;
 
 public class AsignarArbitros {
-	Fecha[] fechas = null;
 	
+	Fecha[] fechas = null;
+	static Set<Integer> arbitrosMarcados = new HashSet<Integer>();
+	
+
+	public static void asignarArbitros(Fecha f) {
+		arbitrosMarcados = new HashSet<Integer>();
+		for (Partido partido : f.getPartidos()) {
+			asignarArbitroAPartido(partido,arbitrosMarcados);
+		}
+	}
+	
+	public static void asignarArbitroAPartido(Partido partido,Set<Integer> arbitrosMarcados) {
+		Equipo equipoa = partido.getEquipo1();
+		Equipo equipob = partido.getEquipo2();
+		
+		int promedio = 100000000; //temporal
+		int arbitroElegido = -1;
+		
+		for (int i = 0 ; i< equipoa.arbitros.length ; i++) {
+			
+			if(equipoa.arbitros[i] + equipob.arbitros[i] < promedio && !arbitrosMarcados.contains(i)) { 
+				arbitroElegido = i;
+				promedio = equipoa.arbitros[i] + equipob.arbitros[i];
+			}
+		}
+		if(arbitroElegido ==-1) {
+			throw new RuntimeException("No se pudo asignar el arbitro");
+		}
+		partido.setArbitro(arbitroElegido);
+		arbitrosMarcados.add(arbitroElegido);
+
+	}
 	
 	
 }
--- tp3_progra3/src/test/AsignarArbitrosTest.java
+++ tp3_progra3/src/test/AsignarArbitrosTest.java
@@ -1,27 +1,116 @@
-package metodos;
+package test;
 
 import static org.junit.Assert.*;
 
-import org.junit.Before;
+import java.util.HashSet;
+
+import org.junit.Ignore;
 import org.junit.Test;
 
 import estructuraDeDatos.Equipo;
+import estructuraDeDatos.Fecha;
 import estructuraDeDatos.Partido;
+import metodos.AsignarArbitros;
 
 public class AsignarArbitrosTest {
 
-	@Before
-	public void setUp() throws Exception {
+	@Test  
+	public void testAsignarArbitros() {
+		Equipo a = new Equipo("Boca", 2);
+		Equipo b = new Equipo("river", 2);
+		Equipo c = new Equipo("Independiente", 2);
+		Equipo d = new Equipo("SanLorenzso", 2);
+		Equipo[] equipos = {a,b,c,d};
+		
+		Fecha f = new Fecha(equipos);
+		f.agregarPartido(a, b);
+		f.agregarPartido(c, d);
+		AsignarArbitros.asignarArbitros(f);
+		assertEquals(0, f.getPartido(0).getArbitro());
+		assertEquals(1, f.getPartido(1).getArbitro());
+		System.out.println(f.getPartido(0));
+		System.out.println(f.getPartido(1));
+		
+		System.out.println("-------------");
+		
+		f = new Fecha(equipos);
+		f.agregarPartido(a,c);
+		f.agregarPartido(b,d);
+		AsignarArbitros.asignarArbitros(f);
+		assertEquals(0, f.getPartido(0).getArbitro());
+		assertEquals(1, f.getPartido(1).getArbitro());
+		System.out.println(f.getPartido(0));
+		System.out.println(f.getPartido(1));
+		System.out.println("-------------");
+		
+		f = new Fecha(equipos);
+		f.agregarPartido(a,d);
+		f.agregarPartido(b,c);
+		AsignarArbitros.asignarArbitros(f);
+		System.out.println(f.getPartido(0));
+		System.out.println(f.getPartido(1));
+		assertEquals(0, f.getPartido(0).getArbitro());
+		assertEquals(1, f.getPartido(1).getArbitro());
+		
 	}
 
-	@Test
+	@Test 
 	public void testAsignarArbitro() {
 		Equipo a = new Equipo("Boca", 2);
 		Equipo b = new Equipo("river", 2);
+		a.arbitros[0] = 1;
 		
 		Partido partido = new Partido(a, b);
 		
+		AsignarArbitros.asignarArbitroAPartido(partido,new HashSet<Integer>());
 		
+		assertEquals(1, partido.getArbitro());
+	}
+	
+	@Test 
+	public void testAsignarArbitroMismoPeso() {
+		Equipo equipoa = new Equipo("Boca", 2);
+		Equipo equipoB = new Equipo("river", 2);
+		equipoa.arbitros[0] = 1;
+		equipoa.arbitros[1] = 1;
+		
+		Partido partido = new Partido(equipoa, equipoB);
+		
+		AsignarArbitros.asignarArbitroAPartido(partido,new HashSet<Integer>());
+		
+		assertEquals(0, partido.getArbitro());
+		
+	}
+	
+	@Test 
+	public void testAsignarArbitroPrimeroMasPeso() {
+		Equipo a = new Equipo("Boca", 2);
+		Equipo b = new Equipo("river", 2);
+		a.arbitros[0] = 1;
+		b.arbitros[0] = 1;
+		a.arbitros[1] = 1;
+		
+		Partido partido = new Partido(a, b);
+		
+		AsignarArbitros.asignarArbitroAPartido(partido,new HashSet<Integer>());
+		
+		assertEquals(1, partido.getArbitro());
+		
+	}
+	
+	@Test 
+	public void testAsignarArbitroSegundoMasPeso() {
+		Equipo a = new Equipo("Boca", 2);
+		Equipo b = new Equipo("river", 2);
+		a.arbitros[0] = 1;
+		a.arbitros[1] = 1;
+		b.arbitros[1] = 1;
+		
+		Partido partido = new Partido(a, b);
+
+		AsignarArbitros.asignarArbitroAPartido(partido,new HashSet<Integer>());
+		
+		assertEquals(0, partido.getArbitro());
 		
 	}
 
