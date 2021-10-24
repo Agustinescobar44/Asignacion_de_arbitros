91e4b3a55ab75c9bcc57374738dae68f902a45bc
agustinesco <84880207+agustinesco@users.noreply.github.com>
2021-10-24 19:16:07 -0300

    Princiapal ahora lee el torneo del archivo prueba

--- tp3_progra3/src/interfaz/Principal.java
+++ tp3_progra3/src/interfaz/Principal.java
@@ -3,11 +3,21 @@ package interfaz;
 import estructuraDeDatos.Equipo;
 import estructuraDeDatos.Torneo;
 import metodos.AsignarArbitros;
+import metodos.Jsons;
 
 public class Principal {
 	
-	private static Torneo inicializarTorneo() {
-		Equipo[] equipos = new Equipo[10];
+
+	
+	static Torneo devolverTorneo() {
+		Torneo t = Jsons.leerTorneoDeJson("tp3_progra3\\src\\archivos\\prueba");
+		AsignarArbitros.asignarArbitros(t);
+		System.out.println(t);
+		return t;
+	}
+
+	public static void main(String[] args) {
+		/*Equipo[] equipos = new Equipo[10];
 		Equipo a = new Equipo("Boca", 5);
 		Equipo b = new Equipo("river", 5);
 		Equipo c = new Equipo("independiente", 5);
@@ -31,18 +41,8 @@ public class Principal {
 		equipos[9] = j;
 		
 		Torneo t = new Torneo(equipos);
-		AsignarArbitros.asignarArbitros(t);
-		return t;
-	}
-	
-	static Torneo devolverTorneo() {
-		Torneo s=inicializarTorneo();
-		return s;
-	}
-
-	public static void main(String[] args) {
-		
-
+		Jsons.generarJSON("tp3_progra3\\src\\archivos\\prueba", t);
+	*/
 	}
 
 }
