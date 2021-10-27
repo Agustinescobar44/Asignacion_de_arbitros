f4b0fe990e37f27193edb29fad707966bbaba901
agustinesco <84880207+agustinesco@users.noreply.github.com>
2021-10-27 20:21:26 -0300

    Cambiado la cantidad de equipos en principal

--- tp3_progra3/src/interfaz/Principal.java
+++ tp3_progra3/src/interfaz/Principal.java
@@ -10,39 +10,24 @@ public class Principal {
 
 	
 	static Torneo devolverTorneo() {
-		Torneo t = Jsons.leerTorneoDeJson("tp3_progra3\\src\\archivos\\prueba");
+		Torneo t = Jsons.leerTorneoDeJson("prueba");
 		AsignarArbitros.asignarArbitros(t);
 		System.out.println(t);
 		return t;
 	}
 
 	public static void main(String[] args) {
-		/*Equipo[] equipos = new Equipo[10];
-		Equipo a = new Equipo("Boca", 5);
-		Equipo b = new Equipo("river", 5);
-		Equipo c = new Equipo("independiente", 5);
-		Equipo d = new Equipo("San Lorenzo", 5);
-		Equipo e = new Equipo("Racing", 5);
-		Equipo f = new Equipo("Sacachispas", 5);
-		Equipo g =new Equipo("Sacachispa", 5);
-		Equipo h =new Equipo("Sacachisp", 5);
-		Equipo i =new Equipo("Sacachis", 5);
-		Equipo j =new Equipo("Sacachi", 5);
 		
-		equipos[0] = a;
-		equipos[1] = b;
-		equipos[2] = c;
-		equipos[3] = d;
-		equipos[4] = e;
-		equipos[5] = f;
-		equipos[6] = g;
-		equipos[7] = h;
-		equipos[8] = i;
-		equipos[9] = j;
+		Equipo a = new Equipo("Boca", 2);
+		Equipo b = new Equipo("river", 2);
+		Equipo c = new Equipo("independiente", 2);
+		Equipo d = new Equipo("San Lorenzo",2);
 		
+		Equipo[] equipos = {a,b,c,d};
+
 		Torneo t = new Torneo(equipos);
-		Jsons.generarJSON("tp3_progra3\\src\\archivos\\prueba", t);
-	*/
+		Jsons.generarJSON("equipos prueba", equipos);
+	
 	}
 
 }
