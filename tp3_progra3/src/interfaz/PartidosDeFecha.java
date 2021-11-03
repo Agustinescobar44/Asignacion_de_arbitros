a2e1a76908f3c0fd61e3775a49b6e8f78d25b9b7
elizabethmg1 <with_a_kiss@hotmail.com>
2021-11-02 21:30:33 -0300

    acomodado el tamanio de las columnas del Jtable

--- tp3_progra3/src/interfaz/PartidosDeFecha.java
+++ tp3_progra3/src/interfaz/PartidosDeFecha.java
@@ -8,8 +8,12 @@ import estructuraDeDatos.Fecha;
 import estructuraDeDatos.Partido;
 
 import javax.swing.JTable;
+import javax.swing.table.TableCellRenderer;
+import javax.swing.table.TableColumnModel;
+
 import java.awt.event.ActionListener;
 import java.awt.Color;
+import java.awt.Component;
 import java.awt.event.ActionEvent;
 import java.awt.Font;
 
@@ -59,7 +63,7 @@ public class PartidosDeFecha {
 
 		JLabel lblNewLabel = new JLabel("Partidos de Fecha: " + (i+1));
 
-		lblNewLabel.setBounds(137, 15, 118, 14);
+		lblNewLabel.setBounds(137, 15, 178, 15);
 		frame.getContentPane().add(lblNewLabel);
 		
 		JButton botonCambiarArbitro = new JButton("Cambiar Arbitro");
@@ -88,6 +92,10 @@ public class PartidosDeFecha {
 		table.setFont(new Font("Tahoma", Font.BOLD, 11));
 		table.setBackground(new Color(0, 128, 0));
 		table.setGridColor(Color.WHITE);
+		
+		table.setAutoResizeMode(JTable.AUTO_RESIZE_ALL_COLUMNS);
+		table.setRowHeight(50);
+		
 		scrollPane.setViewportView(table);
 	}
 	
@@ -112,4 +120,20 @@ public class PartidosDeFecha {
 	public void setVisible() {
 		frame.setVisible(true);
 	}
+	
+	public void resizeColumnWidth(JTable table) {
+	    final TableColumnModel columnModel = table.getColumnModel();
+	    for (int column = 0; column < table.getColumnCount(); column++) {
+	        int width = 15; // Min width
+	        for (int row = 0; row < table.getRowCount(); row++) {
+	            TableCellRenderer renderer = table.getCellRenderer(row, column);
+	            Component comp = table.prepareRenderer(renderer, row, column);
+	            width = Math.max(comp.getPreferredSize().width +1 , width);
+	        }
+	        if(width > 300)
+	            width=300;
+	        columnModel.getColumn(column).setPreferredWidth(width);
+	    }
+	}
+	
 }
