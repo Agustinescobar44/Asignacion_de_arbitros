c669860933c81c29808f769c46f563fd6538d480
agustinesco <84880207+agustinesco@users.noreply.github.com>
2021-11-06 16:58:17 -0300

    Color y fuente

--- tp3_progra3/src/interfaz/Estadisticas.java
+++ tp3_progra3/src/interfaz/Estadisticas.java
@@ -11,12 +11,18 @@ import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import javax.swing.JTable;
+import javax.swing.table.JTableHeader;
+
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Cursor;
 
 public class Estadisticas {
 
 	private JFrame frame;
 	private JTable table;
 	private String pathAImagenFondo ="tp3_progra3\\src\\imagenes\\cancha.jpg"; 
+	private Font fuentePrincipal= new Font("Leelawadee UI", Font.PLAIN, 16);
 
 
 	/**
@@ -33,6 +39,8 @@ public class Estadisticas {
 		frame.setResizable(false);
 		
 		JButton botonVolver = new JButton("Volver");
+		botonVolver.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+		botonVolver.setFont(fuentePrincipal);
 		botonVolver.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				ventanaPrincipal.setVisible(true);
@@ -43,6 +51,7 @@ public class Estadisticas {
 		frame.getContentPane().add(botonVolver);
 		
 		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setFont(fuentePrincipal);
 		scrollPane.setBounds(44, 68, 663, 337);
 		frame.getContentPane().add(scrollPane);
 		
@@ -56,6 +65,17 @@ public class Estadisticas {
 		}
 		
 		table = new JTable(data , encabezado);
+		JTableHeader header = table.getTableHeader(); 
+		
+		header.setFont(fuentePrincipal.deriveFont(Font.BOLD));
+		header.setOpaque(false);
+		header.setBackground(new Color(0, 128, 0));
+		
+		table.setCellSelectionEnabled(false);
+		table.setFont(fuentePrincipal);
+		table.setFillsViewportHeight(true);
+		table.setAutoResizeMode(JTable.AUTO_RESIZE_ALL_COLUMNS);
+		table.setRowHeight(30);
 		
 		scrollPane.setViewportView(table);
 	
@@ -108,6 +128,7 @@ public class Estadisticas {
 		}
 		return ret;
 	}
+	
 
 	public void setVisible() {
 		frame.setVisible(true);
