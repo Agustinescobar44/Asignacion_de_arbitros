10d2cd90652f438aa9d21f35a8a3c0a16c6b67c7
agustinesco <84880207+agustinesco@users.noreply.github.com>
2021-11-07 18:42:15 -0300

    agregados generacion de 16 equipos

--- tp3_progra3/src/interfaz/Principal.java
+++ tp3_progra3/src/interfaz/Principal.java
@@ -17,11 +17,28 @@ public class Principal {
 
 	public static void main(String[] args) {
 		
-		String[] equipos = {"Boca", "River", "Independiente", "Racing", "San Lorenzo", "Huracan", "Central", "Newells", "Gimnasia", "Estudiantes"};
+		String[] diezEquipos = crear10Equipos();
+		String[] cuatroEquipos = crear4Equipos();
+		String[] diezYSeisEquipos = crear16Equipos();
 
-		Torneo t = new Torneo(equipos);
-		Jsons.generarJSON("prueba", t);
+		Torneo torneoDe16 = new Torneo(diezYSeisEquipos);
+		Torneo torneoDe10 = new Torneo(diezEquipos);
+		Torneo torneoDe4 = new Torneo(cuatroEquipos);
+		Jsons.generarJSON("Torneo con 16 equipos", torneoDe16);
+		Jsons.generarJSON("Torneo con 10 equipos", torneoDe10);
+		Jsons.generarJSON("Torneo con 4 equipos", torneoDe4);
 	
+	}
+	
+	private static String[] crear10Equipos() {
+		return new String[]{"Boca", "River", "Independiente", "Racing", "San Lorenzo", "Huracan", "Central", "Newells", "Gimnasia", "Estudiantes"};
+	}
+	private static String[] crear16Equipos() {
+		return new String[]{"Boca", "River", "Independiente", "Racing", "San Lorenzo", "Huracan", "Central", "Newells", "Gimnasia", "Estudiantes"
+				, "Colon", "Union", "Los pumas", "San Miguel", "Talleres" , "Sacachispas"};
+	}
+	private static String[] crear4Equipos() {
+		return new String[]{"Boca", "River", "Independiente", "Racing"};
 	}
 
 }
